12a27f2f5620af343152dfb2b45e9a9a1100d535
StavGoren <stavgo14@gmail.com>
2023-05-03 20:21:10 +0300

    second commit

--- src/main/java/com/stav/server/logic/CouponsLogic.java
+++ src/main/java/com/stav/server/logic/CouponsLogic.java
@@ -90,12 +90,6 @@ public class CouponsLogic {
     private boolean validateId(int id) throws ServerException {
         ValidationsUtils.validateNumber(id);
 
-        List<Coupon> couponList = couponsDal.getAllCoupons();
-        for (int i = 0; i < couponList.size(); i++) {
-            if(couponList.get(i).getId() == id) {
-                return true;
-            }
-        }
         throw new ServerException(ErrorType.DATA_NOT_FOUND, " please make sure you entered a correct id");
     }
 
